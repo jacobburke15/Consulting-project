c51f7f1948b158cf79e5b9bc3906f074bf39d506
Jacob Burke <jacobburke15@gmail.com>
2019-09-18 14:08:35 -0700

    commit

--- Rcodegit.R
+++ Rcodegit.R
@@ -2,3 +2,24 @@
 plantdata <- PlantGrowth
 
 
+library(dplyr)
+
+group1 <- filter(plantdata, group == 'ctrl')
+
+group2 <- filter(plantdata, group == 'trt1')
+
+group3 <- filter(plantdata, group == 'trt2')
+
+t.test(group1$weight, group2$weight)
+
+t.test(group1$weight, group3$weight)
+
+plantdata
+
+fert1 <- c(rep(0, 10), rep(1, 10), rep(0, 10))
+fert2 <- c(rep(0,10), rep(0,10), rep(1,10))
+plantdata <- cbind(plantdata, fert1, fert2)
+plantdata 
+
+reg <- lm(weight ~ fert1 + fert2, data = plantdata)
+summary(reg)
